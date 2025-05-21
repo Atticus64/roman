5b507f06550f755de77d5fad125201ecee40135b
Jonathan <jonathanelian64@gmail.com>
2025-05-21 11:29:39 -0700

    feat: add styles for scan test btn

--- src/main.js
+++ src/main.js
@@ -9,15 +9,52 @@ import { createCard } from './card';
 const socket = io("http://localhost:5000"); // Asegúrate que es el mismo puerto del backend
 const box = document.querySelector(".data");
 
+const message = document.querySelector(".msgBox");
+let cardPresent = false
 
 let atroposInstance = null;
 const btn = document.querySelector(".colorBtn");
+const test = document.querySelector(".testBtn");
 
 let color = "blue";
 let id = 0;
 socket.on("connect", () => {
     console.log("Conectado al servidor WebSocket");
     socket.send("Hola desde el frontend");
+});
+
+test.addEventListener("click", async () => {
+
+    message.textContent = "Esperando escaneo de la tarjeta....."
+    const resp = await fetch("http://localhost:5000/interact")
+    const data = await resp.json();
+
+    cardPresent = data.cardPresent
+    btn.classList.remove("hidden")
+    const cardColor = `bg-${color}-500/50`
+    const border = `border-${color}-500/10`
+    const shadowColor = `shadow-${color}-500/25`
+    const cardContainer = document.querySelector(".card-container");
+    id = data.id;
+
+    cardContainer.innerHTML = createCard({
+        cardColor,
+        border,
+        shadowColor,
+        idCard: id,
+    });
+
+    atroposInstance = atropos({
+        el: ".atropos",
+        rotateTouch: true,
+        rotateXMax: 20,
+        rotateYMax: 20,
+        rotateXFactor: 1,
+        rotateYFactor: 1,
+    });
+
+    message.textContent = ""
+
 });
 
 btn.addEventListener("click", () => {
@@ -31,7 +68,6 @@ btn.addEventListener("click", () => {
     const border = `border-${color}-500/10`
     const shadowColor = `shadow-${color}-500/25`
 
-    console.log(card)
     card.innerHTML = createCard({
         cardColor,
         border,
