8135ba6dadd57b2531c4d89233a247612eb13cd6
Jonathan <jonathanelian64@gmail.com>
2025-05-25 23:12:44 -0700

    feat: add page to get by nfc element

--- src/card.js
+++ src/card.js
@@ -9,14 +9,14 @@ export function createCard({ border, shadowColor, cardColor, idCard }) {
                         <div class="h-full opacity-100 isolate md:aspect-[2/1]">
                             <div class="h-full">
                                 <div
-                                    class="atropos block w-full h-auto mx-auto shadow-2xl md:aspect-[2/1] [box-sizing:border-box] atropos-rotate-touch">
+                                    class="atropos block w-full h-auto mx-auto md:aspect-[2/1] [box-sizing:border-box] atropos-rotate-touch">
                                     <span class="atropos-scale"
                                         style="transform: translate3d(0px, 0px, 0px); transition-duration: 300ms;"><span
                                             class="atropos-rotate"
                                             style="transition-duration: 300ms; transform: translate3d(0px, 0px, 0px) rotateX(0deg) rotateY(0deg);"><span
                                                 class="atropos-inner backdrop-blur-xl rounded-[60px]">
                                                 <div
-                                                    class="block h-full overflow-hidden opacity-100 rounded-[60px] shadow-[inset_0_4px_30px] bg-transparent border p-5 aspect-none w-full md:aspect-[2/1] ${border} ${shadowColor} transition duration-500 ease-in-out">
+                                                    class="block h-full overflow-hidden opacity-100 rounded-[60px] border p-5 aspect-none w-full md:aspect-[2/1] ${border} ${shadowColor} transition duration-400 ease-in-out">
                                                     <div
                                                         class="relative h-full overflow-hidden border rounded-[40px] grid md:flex ${cardColor} border-red-300/20 transition duration-500 ease-in-out">
                                                         <div
@@ -34,7 +34,7 @@ export function createCard({ border, shadowColor, cardColor, idCard }) {
                                                                         class="flex justify-start gap-4 text-white gap-y-2 p-5 flex-col md:items-start md:flex-row md:p-6 items-center text-center md:text-left">
                                                                         <div>
                                                                             <p class="text-xl font-bold"><span
-                                                                                    class="opacity-75 text-midu-primary">#</span>miduConf
+                                                                                    class="opacity-75 text-midu-primary">#</span>romanCard
                                                                             </p><span
                                                                                 class="block text-sm font-normal w-max text-white/60">Evento
                                                                                 de Programación Gratuito</span>
@@ -42,33 +42,18 @@ export function createCard({ border, shadowColor, cardColor, idCard }) {
                                                                     </div>
                                                                 </div>
                                                                 <div
-                                                                    class="items-center gap-4 p-5 flex md:block flex-col-reverse md:flex-row">
-                                                                    <svg preserveAspectRatio="xMidYMid"
-                                                                        viewBox="0 0 228 198"
-                                                                        xmlns="http://www.w3.org/2000/svg" width="1em"
-                                                                        height="1em"
-                                                                        class="order-1 h-auto w-9 ml-0 md:ml-auto">
-                                                                        <g>
-                                                                            <path fill="#199afc"
-                                                                                d="M73.2 126.4c-15 15-15 39.3 0 54.3L19 126.4C4 111.5 4 87.2 19 72.2L73.2 18c15-15 39.2-15 54.2 0s15 39.2 0 54.2z">
-                                                                            </path>
-                                                                            <path fill="#1d5682"
-                                                                                d="m73.2 126.4 27.1-27.1 27.1 27.1c15 15 15 39.3 0 54.3-15 14.9-39.2 14.9-54.2 0-15-15-15-39.3 0-54.3z">
-                                                                            </path>
-                                                                            <g>
-                                                                                <path fill="#199afc"
-                                                                                    d="M185.5 84.3c8.3-8.2 8.3-21.7 0-29.9l30 29.9c8.2 8.3 8.2 21.7 0 30l-30 30c-8.3 8.3-21.7 8.3-30 0s-8.3-21.7 0-30z">
-                                                                                </path>
-                                                                                <path fill="#1d5682"
-                                                                                    d="m185.5 84.3-15 15-15-15c-8.3-8.2-8.3-21.7 0-29.9 8.3-8.3 21.7-8.3 30 0 8.3 8.2 8.3 21.7 0 29.9z">
-                                                                                </path>
-                                                                            </g>
-                                                                        </g>
-                                                                    </svg><time datetime="2024-09-12T06:00:00"
-                                                                        class="block mt-2 ml-auto font-bold text-white md:ml-0 text-center mr-auto md:mr-0 md:text-right">Sept.
-                                                                        12 2024<span
-                                                                            class="block text-sm font-normal text-white/60 animate-blurred-fade-in">8:00
-                                                                            GMT-7</span></time></div>
+                                                                    class="gap-4 p-5 flex justify-items-end  md:block flex-col-reverse md:flex-row">
+                                                                    <img src="/uas.png" class="w-15">
+                                                                    <time datetime="${new Date().toISOString()}"
+                                                                        class="block mt-2 ml-auto font-bold text-white md:ml-0 text-center mr-auto md:mr-0 md:text-right">${new Date().toDateString()}<span
+                                                                            class="block text-sm font-normal text-white/60 animate-blurred-fade-in">${new Intl.DateTimeFormat('en-GB', {
+                                                                                timeZone: 'UTC',
+                                                                                hour12: false,
+                                                                                hour: '2-digit',
+                                                                                minute: '2-digit',
+                                                                                second: '2-digit'
+                                                                            }).format(new Date())} GTM</span><span
+                                                                            });}</span></time></div>
                                                             </div>
                                                             <div
                                                                 class="flex flex-row-reverse items-center w-auto h-auto gap-2 mx-auto md:ml-0 justify-center md:justify-self-end md:mr-4">
@@ -103,7 +88,7 @@ export function createCard({ border, shadowColor, cardColor, idCard }) {
                                                     </div>
                                                 </div><span class="atropos-highlight"
                                                     style="transform: translate3d(0px, 0px, 0px); transition-duration: 300ms; opacity: 0;"></span>
-                                            </span><span class="atropos-shadow"
+                                            </span><span class=""
                                                 style="transform: translate3d(0px, 0px, -50px) scale(1); transition-duration: 300ms;"></span></span></span>
                                 </div>
                             </div>
--- src/consultar.js
+++ src/consultar.js
@@ -1,5 +1,10 @@
+import { toast } from "wc-toast";
+import { createCard } from "./card";
+import atropos from "atropos";
+import 'atropos/css'
 
-
+let atroposInstance = null;
+let color = 'blue';
 async function getTypeRead(id) {
     const response = await fetch('http://localhost:5000/get_type_read', {
         method: 'POST',
@@ -14,25 +19,77 @@ async function getTypeRead(id) {
     console.log(data);
 }
 
+/**
+ * 
+ * @returns { id: string, ok: boolean }
+ */
 async function leerScaner() {
-    const response = await fetch('http://localhost:8080/interact_fake')
-    const data = await response.json()
+    try {
+        const response = await fetch('http://localhost:8080/interact_fake')
+        const data = await response.json()
 
-    const consulta = await fetch('http://localhost:5000/get_type_read', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            id: data.id
+        const consulta = await fetch('http://localhost:5000/get_type_read', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                id: data.id
+            })
         })
-    })
-    const dataConsulta = await consulta.json()
-    console.log(dataConsulta);
-    //const resp = await fetch(`http://localhost:5000/users/${data.id}`)
-    const resp = await fetch(`http://localhost:5000/users/nfc/${data.id}`)
-    const user = await resp.json()
-    return user
+
+        const dataConsulta = await consulta.json()
+        if (!dataConsulta.ok) {
+            toast.error('Error al consultar el tipo de lectura');
+            return
+        }
+
+        const cardContainer = document.querySelector(".card-container");
+        if (dataConsulta.type == 'user') {
+            toast.success('Tipo de lectura: Usuario')
+            const resp = await fetch(`http://localhost:5000/users/nfc/${data.id}`)
+            const user = await resp.json()
+            const cardColor = `bg-${color}-500/50`
+            const border = `border-${color}-500/10`
+            const shadowColor = `shadow-${color}-500/25`
+
+            const card = createCard({
+                border,
+                shadowColor,
+                cardColor,
+                idCard: data.id
+            })
+            cardContainer.innerHTML = card; 
+            atroposInstance = atropos({
+                el: ".atropos",
+                rotateTouch: true,
+                rotateXMax: 20,
+                rotateYMax: 20,
+                rotateXFactor: 1,
+                rotateYFactor: 1,
+            });
+
+        } else if (dataConsulta.type == 'book') {
+            const resp = await fetch(`http://localhost:5000/books/nfc/${data.id}`)
+            const { book: libro } = await resp.json()
+            cardContainer.innerHTML = `
+            <div class="flex flex-col items-center justify-center p-6 bg-white rounded-lg shadow-md max-w-md mx-auto">
+                <h2>${libro.nombre}</h2>
+                <p><strong>Autor:</strong> ${libro.autor}</p>
+                <p><strong>Año:</strong> ${libro.year}</p>
+                <p><strong>ISBN:</strong> ${libro.isbn}</p>
+                <img src="${libro.portada_url}" alt="${libro.nombre}" class="w-[15rem]" style="border-radius: 8px; margin-top: 10px;">
+            </div>
+          `;
+            toast.success('Tipo de lectura: Libro');
+        }
+        return dataConsulta;
+    }
+    catch (error) {
+        console.error('Error al leer el escáner:', error);
+        toast.error('Error al leer el escáner');
+        return null;
+    }
 }
 
 
@@ -44,4 +101,5 @@ btnStartNFC.addEventListener('click', async () => {
 
     const user = await leerScaner()
     console.log(user);
+    btnStartNFC.disabled = false;
 })
